6aa3f02e682a3e455171956443294405d93c1a0f
juanruizkk <juanignacioruizr@gmail.com>
2024-11-12 22:47:41 -0300

    Formulario creacion de persona termiando

--- script.js
+++ script.js
@@ -127,8 +127,27 @@ persona1.mostrarGeneracion(); // Salida: Juan pertenece a la Generación Z y su 
 const esMayor = (edad) => edad > 18 ? `${this.nombre} es mayor de edad` : `${this.nombre} no es mayor de edad`;
 
 const mostrarDatos = () => {
-    `${this.nombre},
-     ${this.edad}, 
-     ${this.dni},${this.sexo},
-     ${this.peso},${this.altura},${this.anioNacimiento}.`
+    `${this.nombre}, ${this.edad}, ${this.dni}, ${this.sexo}, ${this.peso}, ${this.altura}, ${this.anioNacimiento}.`
 }
+
+
+/* Validar form */
+
+(() => {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+
+            form.classList.add('was-validated')
+        }, false)
+    })
+})()
